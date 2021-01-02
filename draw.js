182e18dac48d15e478d162a1fa5f0ea2efeac136
Corey J <coreyajeffers@gmail.com>
2021-01-02 17:00:23 -0500

    Airbnb cleanup
    Cleaning up the autofixables

--- draw.js
+++ draw.js
@@ -1,100 +1,83 @@
-function draw() { //Draws objects to the canvas
+function draw() { // Draws objects to the canvas
+  canvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
-    canvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  if (currentState === states.splash) {
+    canvas.fillStyle = '#000'; // Set color to black
+    canvas.font = '25pt Calibri';
+    const SPLASH_SCREEN_TEXT = 'Team Splash Screen';
+    splashTextX = canvas.measureText(SPLASH_SCREEN_TEXT).width;
+    canvas.fillText(SPLASH_SCREEN_TEXT, (CANVAS_WIDTH / 2) - (splashTextX / 2), splashTextY);
+  }
 
-    if (currentState === states.splash) {
+  if (currentState === states.title) {
+    canvas.fillStyle = '#000'; // Set color to black
+    canvas.font = 'bold 40pt Calibri';
+    const GAME_NAME_TEXT = 'GAME NAME';
+    gameTextx = canvas.measureText(GAME_NAME_TEXT).width; // Centers the text based on length
+    canvas.fillText(GAME_NAME_TEXT, (CANVAS_WIDTH / 2) - (gameTextx / 2) - 3, CANVAS_HEIGHT / 3);
+    // The next two create a special text effect
+    canvas.fillStyle = '#F00';
+    canvas.fillText(GAME_NAME_TEXT, (CANVAS_WIDTH / 2) - (gameTextx / 2), CANVAS_HEIGHT / 3);
 
-        canvas.fillStyle = "#000"; // Set color to black
-        canvas.font = '25pt Calibri';
-        var SPLASH_SCREEN_TEXT = "Team Splash Screen";
-        splashTextX = canvas.measureText(SPLASH_SCREEN_TEXT).width;
-        canvas.fillText(SPLASH_SCREEN_TEXT, (CANVAS_WIDTH / 2) - (splashTextX / 2), splashTextY);
-    }
+    canvas.fillStyle = '00F';
+    canvas.fillText(GAME_NAME_TEXT, (CANVAS_WIDTH / 2) - (gameTextx / 2) + 3, CANVAS_HEIGHT / 3);
 
-    if (currentState === states.title) {
+    canvas.fillStyle = '#F00';
+    canvas.font = 'bold 20pt Calibri';
+    const SPACEBAR_TEXT = 'Press Space to Continue';
+    spaceBarTextx = canvas.measureText(SPACEBAR_TEXT).width; // Centers the text based on length
+    canvas.fillText(SPACEBAR_TEXT, (CANVAS_WIDTH / 2) - (spaceBarTextx / 2), CANVAS_HEIGHT - CANVAS_HEIGHT / 4);
+  }
 
-        canvas.fillStyle = "#000"; // Set color to black
-        canvas.font = 'bold 40pt Calibri';
-        var GAME_NAME_TEXT = "GAME NAME";
-        gameTextx = canvas.measureText(GAME_NAME_TEXT).width; //Centers the text based on length
-        canvas.fillText(GAME_NAME_TEXT, (CANVAS_WIDTH / 2) - (gameTextx / 2) - 3, CANVAS_HEIGHT / 3);
-        //The next two create a special text effect
-        canvas.fillStyle = "#F00";
-        canvas.fillText(GAME_NAME_TEXT, (CANVAS_WIDTH / 2) - (gameTextx / 2), CANVAS_HEIGHT / 3);
+  if (currentState === states.Game) {
+    parallax.Draw(); // draw background
+    player.draw();
 
-        canvas.fillStyle = "00F";
-        canvas.fillText(GAME_NAME_TEXT, (CANVAS_WIDTH / 2) - (gameTextx / 2) + 3, CANVAS_HEIGHT / 3);
+    tileArray.forEach((tile) => {
+      tile.draw();
+    });
 
-        canvas.fillStyle = "#F00";
-        canvas.font = 'bold 20pt Calibri';
-        var SPACEBAR_TEXT = "Press Space to Continue";
-        spaceBarTextx = canvas.measureText(SPACEBAR_TEXT).width; //Centers the text based on length
-        canvas.fillText(SPACEBAR_TEXT, (CANVAS_WIDTH / 2) - (spaceBarTextx / 2), CANVAS_HEIGHT - CANVAS_HEIGHT / 4);
+    playerBullets.forEach((bullet) => {
+      bullet.draw();
+    });
 
-    }
+    playerMissiles.forEach((missle) => {
+      missle.draw();
+    });
 
-    if (currentState === states.Game) {
-        parallax.Draw(); //draw background
-        player.draw();
+    // Enemy Draw
+    enemies.forEach((enemy) => {
+      enemy.draw();
+    });
 
-        tileArray.forEach(function (tile) {
-            tile.draw();
-        });
+    // Life Bar top is pink static background
+    canvas.strokeRect(20, 20, 100 * 2, 10);
+    canvas.fillStyle = '#8B8989';
+    canvas.fillRect(20, 20, 100 * 2, 10);
 
-        playerBullets.forEach(function (bullet) {
-            bullet.draw();
-        });
+    // Second bar is red dynamic one
+    canvas.strokeRect(20, 20, 100 * 2, 10);
+    canvas.fillStyle = '#F00';
+    canvas.fillRect(20, 20, player.life * 2, 10);
+  }
 
-        playerMissiles.forEach(function (missle) {
-            missle.draw();
-        });
+  if (currentState === states.End) {
+    canvas.fillStyle = '#F00'; // Set color to red
+    canvas.font = '25pt Calibri';
 
-        //Enemy Draw
-        enemies.forEach(function (enemy) {
-            enemy.draw();
-        });
+    const GameOVER_TEXT = 'Game Over';
+    endTextX = canvas.measureText(GameOVER_TEXT).width; // Centers the text based on length
+    // canvas.fillText(GameOVER_TEXT, (CANVAS_WIDTH/2) - (GameOVER_TEXTx/2) , CANVAS_HEIGHT-CANVAS_HEIGHT/4);
 
-        //Life Bar top is pink static background
-        canvas.strokeRect(20, 20, 100 * 2, 10);
-        canvas.fillStyle = "#8B8989";
-        canvas.fillRect(20, 20, 100 * 2, 10);
+    canvas.fillText(GameOVER_TEXT, (CANVAS_WIDTH / 2) - (endTextX / 2), endTextY - 90);
 
-        //Second bar is red dynamic one
-        canvas.strokeRect(20, 20, 100 * 2, 10);
-        canvas.fillStyle = "#F00";
-        canvas.fillRect(20, 20, player.life * 2, 10);
+    canvas.fillStyle = '#000'; // Set color to black
+    canvas.font = '20pt Calibri';
+    endTextX = canvas.measureText('First Firstnameson').width;
+    canvas.fillText('First Firstnameson', (CANVAS_WIDTH / 2) - (endTextX / 2), endTextY - 45);
 
-
-    }
-
-
-
-    if (currentState === states.End) {
-
-
-        canvas.fillStyle = "#F00"; // Set color to red
-        canvas.font = '25pt Calibri';
-
-        var GameOVER_TEXT = "Game Over";
-        endTextX = canvas.measureText(GameOVER_TEXT).width; //Centers the text based on length
-        //canvas.fillText(GameOVER_TEXT, (CANVAS_WIDTH/2) - (GameOVER_TEXTx/2) , CANVAS_HEIGHT-CANVAS_HEIGHT/4);
-
-        canvas.fillText(GameOVER_TEXT, (CANVAS_WIDTH / 2) - (endTextX / 2), endTextY - 90);
-
-
-        canvas.fillStyle = "#000"; // Set color to black
-        canvas.font = '20pt Calibri';
-        endTextX = canvas.measureText("First Firstnameson").width;
-        canvas.fillText("First Firstnameson", (CANVAS_WIDTH / 2) - (endTextX / 2), endTextY - 45);
-
-
-        canvas.fillStyle = "#000"; // Set color to black
-        canvas.font = '20pt Calibri';
-        canvas.fillText("Second Secondton", (CANVAS_WIDTH / 2) - (endTextX / 2), endTextY);
-
-
-
-    }
-
-
+    canvas.fillStyle = '#000'; // Set color to black
+    canvas.font = '20pt Calibri';
+    canvas.fillText('Second Secondton', (CANVAS_WIDTH / 2) - (endTextX / 2), endTextY);
+  }
 }
--- update.js
+++ update.js
@@ -1,136 +1,95 @@
-function update() { //Updates location and reaction of objects to the canvas
+function update() { // Updates location and reaction of objects to the canvas
+  if (currentState === states.splash) {
+    // splashTextX += 1;
+    splashTextY += 1;
 
+    if (splashTextY >= 300) {
+      currentState = states.title;
+    }
+  }
 
-    if (currentState === states.splash) {
+  if (currentState === states.title) {
+    if (keydown.space) {
+      currentState = states.Game;
+    }
+  }
 
-        //splashTextX += 1;
-        splashTextY += 1;
-
-        if (splashTextY >= 300) {
-
-            currentState = states.title;
-        }
-
-
+  if (currentState === states.Game) {
+    // Player Movement Controls
+    if (keydown.left) {
+      if (player.velX > -player.speed) {
+        player.velX--;
+      }
     }
 
-
-
-    if (currentState === states.title) {
-
-        if (keydown.space) {
-
-            currentState = states.Game;
-        }
-
-
+    if (keydown.right) {
+      if (player.velX < player.speed) {
+        player.velX++;
+      }
     }
 
-
-    if (currentState === states.Game) {
-
-
-        //Player Movement Controls
-        if (keydown.left) {
-            if (player.velX > -player.speed) {
-                player.velX--;
-            }
-        }
-
-        if (keydown.right) {
-            if (player.velX < player.speed) {
-                player.velX++;
-            }
-        }
-
-        if (keydown.up) {
-            if (player.velY > -player.speed) {
-                player.velY--;
-            }
-
-
-        }
-
-        if (keydown.down) {
-            if (player.velY < player.speed) {
-                player.velY++;
-            }
-
-        }
-
-        player.velX *= player.friction;
-        player.x += player.velX;
-        player.velY *= player.friction;
-        player.y += player.velY;
-
-        player.x = player.x.clamp(0, CANVAS_WIDTH - player.width); //prevents character from going past canvas
-
-
-        player.y = player.y.clamp(0, CANVAS_HEIGHT - player.height); //prevents character from going past canvas
-
-
-        //Player actions
-        if (keydown.space) {
-            player.shoot();
-        }
-
-
-        playerBullets.forEach(function (bullet) {
-            bullet.update();
-        });
-
-        playerBullets = playerBullets.filter(function (bullet) {
-            return bullet.active;
-        });
-
-
-        if (keydown.v) {
-            player.launch();
-        }
-
-        playerMissiles.forEach(function (missle) {
-            missle.update();
-        });
-
-        playerMissiles = playerMissiles.filter(function (missle) {
-            return missle.active;
-        });
-
-
-        //Enemy Update logic
-        enemies.forEach(function (enemy) {
-            enemy.update();
-        });
-
-        enemies = enemies.filter(function (enemy) {
-            return enemy.active;
-        });
-
-        if (Math.random() < 0.1) {
-            enemies.push(Enemy());
-        }
-
-        //Handle Collision
-        handleCollisions();
-
-
-
-
+    if (keydown.up) {
+      if (player.velY > -player.speed) {
+        player.velY--;
+      }
     }
 
-
-
-    if (currentState === states.End) {
-
-
-        endTextY = endTextY + 1;
-
-        if (endTextY >= 300) {
-
-            endTextY = 300;
-        }
-
+    if (keydown.down) {
+      if (player.velY < player.speed) {
+        player.velY++;
+      }
     }
 
+    player.velX *= player.friction;
+    player.x += player.velX;
+    player.velY *= player.friction;
+    player.y += player.velY;
 
+    player.x = player.x.clamp(0, CANVAS_WIDTH - player.width); // prevents character from going past canvas
+
+    player.y = player.y.clamp(0, CANVAS_HEIGHT - player.height); // prevents character from going past canvas
+
+    // Player actions
+    if (keydown.space) {
+      player.shoot();
+    }
+
+    playerBullets.forEach((bullet) => {
+      bullet.update();
+    });
+
+    playerBullets = playerBullets.filter((bullet) => bullet.active);
+
+    if (keydown.v) {
+      player.launch();
+    }
+
+    playerMissiles.forEach((missle) => {
+      missle.update();
+    });
+
+    playerMissiles = playerMissiles.filter((missle) => missle.active);
+
+    // Enemy Update logic
+    enemies.forEach((enemy) => {
+      enemy.update();
+    });
+
+    enemies = enemies.filter((enemy) => enemy.active);
+
+    if (Math.random() < 0.1) {
+      enemies.push(Enemy());
+    }
+
+    // Handle Collision
+    handleCollisions();
+  }
+
+  if (currentState === states.End) {
+    endTextY += 1;
+
+    if (endTextY >= 300) {
+      endTextY = 300;
+    }
+  }
 }
